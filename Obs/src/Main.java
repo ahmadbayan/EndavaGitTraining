56046856ce2116c0c467871965143d6dc86ae73f
Ahmad Bayan <Ahmad.Bayan@endava.com>
2019-03-21 16:24:13 +0200

    MH

---
+++ Obs/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter password : ");
+            String password = keyboard.next();
+
+            int[] encryption = new int[password.length()];
+
+            Vulnerability.encryptionMethod(password` `);
+
+
+
+
+
+
+    }
+}
