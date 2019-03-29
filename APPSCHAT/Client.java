b03f0caf14e8f69d09f1eb68c9ee20ef74da9651
Ahmad <noaa322@gmail.com>
2019-03-29 17:42:17 +0200

    Almost final

--- APPSCHAT/Client.java
+++ APPSCHAT/Client.java
@@ -3,8 +3,11 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client extends JFrame {
@@ -17,24 +20,104 @@ public class Client extends JFrame {
     private String serverIP;
     private Socket connection;
 
-    public Client(String host){
+    public Client(String host) {
         super("Client ");
         serverIP = host;
         userText = new JTextField();
         userText.setEditable(false);
         userText.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                sendData(e.getActionCommand());
+                sendMessage(e.getActionCommand());
                 userText.setText("");
             }
         });
         add(userText, BorderLayout.NORTH);
         chatWindow = new JTextArea();
         add(new JScrollPane(chatWindow), BorderLayout.NORTH);
-        setSize(300,150);
+        setSize(300, 150);
         setVisible(true);
     }
 
-    public
+    public void startRunning() {
+        try {
+            connectToServer();
+            setupStreams();
+            whileChatting();
+        } catch (EOFException eofException) {
+            showMessage("\n Client terminated connection.");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            closeCrap();
+        }
+    }
+
+    private void connectToServer() throws IOException {
+        showMessage("Attempting connection...\n");
+        connection = new Socket(InetAddress.getByName(serverIP), 8080);
+        showMessage("Connected to: " + connection.getInetAddress().getHostName());
+    }
+
+    private void setupStreams() throws IOException {
+        output = new ObjectOutputStream((connection.getOutputStream()));
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());
+        showMessage("\n Stream good to go!\n");
+    }
+
+    private void whileChatting() throws IOException {
+        ableToType(true);
+        do {
+
+            try {
+                message = (String) input.readObject();
+                showMessage("\n" + message);
+            } catch (ClassNotFoundException classNotFoundException) {
+                showMessage("\n Uknown object type.");
+            }
+
+        } while (!message.equals("SERVER - END"));
+    }
+
+
+    private void closeCrap() {
+        showMessage("\n closing everything...");
+        ableToType(false);
+        try {
+            output.close();
+            input.close();
+            connection.close();
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private void sendMessage(String message) {
+        try {
+            output.writeObject("CLIENT - " + message);
+            output.flush();
+            showMessage("\n CLIENT - " + message);
+        } catch (IOException ioException) {
+            chatWindow.append("\n Message not sent!");
+        }
+    }
+
+    private void showMessage(final String m){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                chatWindow.append(m);
+            }
+        });
+    }
+
+    private void ableToType(final  boolean tof){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                userText.setEditable(tof);
+            }
+        });
+    }
 
 }
+
